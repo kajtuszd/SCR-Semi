a029b89218d94a3cc240a5e8aebdf851fe4cf86d
kajetan <kajetanzdanowicz@wp.pl>
2020-11-16 00:21:20 +0100

    added api

---
+++ ex2/code/api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Factory(Resource):
+    
+    def get(self):
+        return {
+            'cars': ['Audi', 'BMW', 'Mercedes',]
+        }
+
+api.add_resource(Factory, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=90, debug=True)
+
+
